ad3db5bc2cc34b0b9d293703ec27d6fa82b3a565
Richard Challis <rjchallis@gmail.com>
2020-10-05 09:55:29 +0100

    load summary data when visible

--- src/client/views/components/HistogramSVG.jsx
+++ src/client/views/components/HistogramSVG.jsx
@@ -5,8 +5,11 @@ import withLookup from "../hocs/withLookup";
 import withSearch from "../hocs/withSearch";
 import withSummary from "../hocs/withSummary";
 import withSummaryById from "../hocs/withSummaryById";
+import styles from "./Styles.scss";
 import Tooltip from "@material-ui/core/Tooltip";
 import { formatter } from "../functions/formatter";
+import { useVisible } from "react-hooks-visible";
+import Skeleton from "@material-ui/lab/Skeleton";
 
 const HistogramSVG = ({
   summaryId,
@@ -18,14 +21,15 @@ const HistogramSVG = ({
   resetLookup,
 }) => {
   const height = 100;
+  const [targetRef, visible] = useVisible();
   let parts = summaryId.split("--");
   useEffect(() => {
-    if (summaryId) {
+    if (summaryId && visible) {
       setTimeout(() => {
         fetchSummary(parts[0], parts[1], parts[2]);
       }, sequence * 100);
     }
-  }, [summaryId]);
+  }, [summaryId, visible]);
   const handleClick = (bucket) => {
     let query = `tax_tree(${parts[0]})`;
     if (bucket.hasOwnProperty("min")) {
@@ -49,7 +53,11 @@ const HistogramSVG = ({
   }
   if (buckets.length > 0 && summaryById.max == 0) {
     return (
-      <svg viewBox={"0 0 1000 25"} preserveAspectRatio="xMinYMin">
+      <svg
+        viewBox={"0 0 1000 25"}
+        preserveAspectRatio="xMinYMin"
+        ref={targetRef}
+      >
         <text
           style={{ fontSize: "12px" }}
           x={1000 / 2}
@@ -62,6 +70,21 @@ const HistogramSVG = ({
           no data
         </text>
       </svg>
+    );
+  }
+  if (buckets.length == 0) {
+    let scale = 0.75;
+    return (
+      <div className={styles.fullWidth} ref={targetRef}>
+        <div
+          style={{
+            transform: `translate(25px, 10px) scale(${scale})`,
+            transformOrigin: "left top",
+          }}
+        >
+          <Skeleton variant="rect" width={1000} height={height} />
+        </div>
+      </div>
     );
   }
   let histogramRects = [];
@@ -133,7 +156,11 @@ const HistogramSVG = ({
     );
   });
   return (
-    <svg viewBox={"-25 -10 1050 135"} preserveAspectRatio="xMinYMin">
+    <svg
+      viewBox={"-25 -10 1050 135"}
+      preserveAspectRatio="xMinYMin"
+      ref={targetRef}
+    >
       <g>{histogramTicks}</g>
       <g>{histogramRects}</g>
       <g>{histogramText}</g>
--- src/client/views/components/WordCloud.jsx
+++ src/client/views/components/WordCloud.jsx
@@ -9,6 +9,8 @@ import Tooltip from "@material-ui/core/Tooltip";
 import { formatter } from "../functions/formatter";
 import { TagCloud } from "react-tagcloud";
 import styles from "./Styles.scss";
+import { useVisible } from "react-hooks-visible";
+import Skeleton from "@material-ui/lab/Skeleton";
 
 const WordCloud = ({
   summaryId,
@@ -20,14 +22,15 @@ const WordCloud = ({
   resetLookup,
 }) => {
   const height = 100;
+  const [targetRef, visible] = useVisible();
   let parts = summaryId.split("--");
   useEffect(() => {
-    if (summaryId) {
+    if (summaryId && visible) {
       setTimeout(() => {
         fetchSummary(parts[0], parts[1], parts[2]);
       }, sequence * 100);
     }
-  }, [summaryId]);
+  }, [summaryId, visible]);
   const handleClick = (bucket) => {
     let query = `tax_tree(${parts[0]}) AND ${parts[1]}=${bucket.value}`;
     updateSearch({ query, searchRawValues: true });
@@ -41,6 +44,34 @@ const WordCloud = ({
   if (summaryById && summaryById.buckets) {
     buckets = summaryById.buckets;
   }
+  if (buckets.length > 0 && summaryById.max == 0) {
+    return (
+      <svg
+        viewBox={"0 0 1000 25"}
+        preserveAspectRatio="xMinYMin"
+        ref={targetRef}
+      >
+        <text
+          style={{ fontSize: "12px" }}
+          x={1000 / 2}
+          y={25 / 2}
+          fillOpacity={0.5}
+          textAnchor="middle"
+          dominantBaseline="central"
+          pointerEvents={"none"}
+        >
+          no data
+        </text>
+      </svg>
+    );
+  }
+  if (buckets.length == 0) {
+    return (
+      <div className={styles.fullWidth} ref={targetRef}>
+        <Skeleton variant="rect" width={400} height={50} />
+      </div>
+    );
+  }
   return (
     <TagCloud
       className={styles.tagCloud}
@@ -49,6 +80,7 @@ const WordCloud = ({
       tags={buckets}
       disableRandomColor
       onClick={handleClick}
+      ref={targetRef}
     />
   );
 };
